6f5d8ece2f8f6438353039b5cf55f58017e8697a
Nga Nguyen <ngasdragon@gmail.com>
2019-11-25 18:20:04 -0800

    deleteuseless lines

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,30 +3,6 @@ import React from "react";
 import "components/InterviewerList.scss"
 import InterviewerListItem from "./InterviewerListItem";
 
-// export default function InterviewerList({interviewers, interviewer, setInterviewer}) {
-//   const interviewerList = interviewers.map((interviewer) =>
-//   <section className={interviewers}>
-//     <h4 className="interviewers__header text--light">Interviewer</h4> 
-
-//     <ul className="interviewers__list">
-//       <InterviewerListItem
-//         key={interviewer.id} //what this for?
-//         name={interviewer.name}
-//         avatar={interviewer.avatar}
-//         selected={interviewer.id === interviewer}
-//         setInterviewer={() => setInterviewer(interviewer.id)}
-//          />
-
-//     </ul>
-//     </section>
-//   );
-  
-//   return interviewerList;
-
-  
-// }
-//h4. where should i render??
-
 export default function InterviewerList({interviewers, value, onChange}) {
   const interviewerList = interviewers.map((interviewer) =>
       <InterviewerListItem
