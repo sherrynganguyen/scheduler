8c93925f7a050c26f437977f2f4454153a61ec9d
Nga Nguyen <ngasdragon@gmail.com>
2019-12-04 11:21:05 -0800

    pass 3rd test for application

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -80,51 +80,12 @@ describe('Application', () => {
     fireEvent.click(getByText(appointment, "Confirm"));
     expect(getByText(appointment, "DELETING")).toBeInTheDocument();
 
-    debug(prettyDOM(appointment))
-    waitForElement(() => getByAltText(appointment, "Add"));
-    debug(prettyDOM(appointment))
+    await waitForElement(() => getByAltText(appointment, "Add"));
     const day = getAllByTestId(container, "day").find(day =>
       queryByText(day, "Monday")
     );
-    debug(prettyDOM(appointment))
     expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
-    debug(prettyDOM(appointment))
   });
-
-// //WORKING ON
-//   it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
-//     // 1. Render the element Application
-//     const { container, debug } = render(<Application />);
-//     // 2. Wait until the text "Archie Cohen" is displayed.
-//     await waitForElement(() => getByText(container,"Archie Cohen"));
-
-
-//     // 3. Click delete button
-//     const appointment = getAllByTestId(container, "appointment").find(
-//       appointment => queryByText(appointment, "Archie Cohen")
-//     )
-
-//     fireEvent.click(queryByAltText(appointment, "Delete"));
-//     // debug()  
-
-//     // 4. Render the element Confirm
-//     await waitForElement(() => getByTestId(container, "confirm"));
-//     // 5. Click confirm button to delete
-//     // debug(prettyDOM(container))
-//     expect(getByText(appointment, "Are you sure you want to delete?")).toBeInTheDocument();
-
-//     fireEvent.click(queryByText(appointment, "Confirm"))
-//     // 6. Check that the element with the text "Deleting" is displayed
-//     expect(getByText(appointment, "DELETING")).toBeInTheDocument();
-//     // 7. Wait until the element Empty displayed
-//     await waitForElement(() => queryByText(container, "Empty"));
-//     // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
-//     const day = getAllByTestId(container, "day").find(day =>
-//       queryByText(day, "Monday")
-//     );
-//     expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
-//   })
-
 
 //   it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
 //     // 1. Render the element Application
