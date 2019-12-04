9ad19ece48b9654daef1e18877fbc75d487651b9
Nga Nguyen <ngasdragon@gmail.com>
2019-12-03 16:53:49 -0800

    initialize four more test for application/index.js

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -37,22 +37,36 @@ describe('Application', () => {
     fireEvent.click(getByText("Tuesday"));
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
     });
-
+  
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
-    const { container, debug } = render(<Application />);
-    await waitForElement(() => getByText(container,"Archie Cohen"))
-    const appointment = getAllByTestId(container, "appointment")[0];
 
+    // 1. Render the Application.
+    const { container, debug } = render(<Application />);
+
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container,"Archie Cohen"))
+
+    // 3. Click the "Add" button on the first empty appointment.
     fireEvent.click(getByAltText(appointment, "Add"));
+
+    // 4. Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
     fireEvent.change(getByPlaceholderText(appointment, /Enter Student Name/),
       { target: { value: 'Lydia Miller-Jones' } }
     );
+
+    // 5. Click the first interviewer in the list.
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    // 6. Click the "Save" button on that same appointment.
     fireEvent.click(getByText(appointment, "Save"))
+
+    // 7. Check that the element with the text "Saving" is displayed.
     expect(getByText(appointment, "SAVING")).toBeInTheDocument();
 
+    // 8. Wait until the element with the text "Lydia Miller-Jones" is displayed.
     await waitForElement(() => queryByText(appointment, "Lydia Miller-Jones")); // this doesnt pass the test due to the WebSocket API
 
+    // 9. Check that the DayListItem with the text "Monday" also has the text "no spots remaining".
     const day = getAllByTestId(container, "day").find(day =>
       queryByText(day, "Monday")
     );
@@ -60,6 +74,24 @@ describe('Application', () => {
     expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
 
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+
+  })
+
+
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+
+  })
+
+
+  it("shows the save error when failing to save an appointment", async () => {
+
+  })
+
+  
+  it("shows the delete error when failing to delete an existing appointment", async () => {
+
+  })
 });
 
 
