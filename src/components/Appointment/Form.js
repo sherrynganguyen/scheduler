93ab8119d56f3a607f248eba02d87e35d2014d2c
Nga Nguyen <ngasdragon@gmail.com>
2019-12-08 19:32:02 -0800

    reverse interviewer validation

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -12,10 +12,6 @@ export default function Form(props) {
       setError("Student name cannot be blank");
       return;
     }
-    if (interviewer === null) {
-      setError("Please select interviewer");
-      return;
-    }
     setError("");
     props.onSave(name, interviewer);
   }
